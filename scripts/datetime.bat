2821812b20e302f7f39815ff2171efc5e735d880
Jinghui Hu <hujinghui@buaa.edu.cn>
2016-10-06 17:15:47 +0800

    add datetime batch tool

---
+++ scripts/datetime.bat
@@ -0,0 +1,12 @@
+:: echo datetime in yyyy-mm-dd HH:MM:SS format
+
+
+@SET year=%DATE:~0,4%
+@SET month=%DATE:~5,2%
+@SET day=%DATE:~8,2%
+
+@SET hour=%TIME:~0,2%
+@SET minute=%TIME:~3,2%
+@SET second=%TIME:~6,2%
+
+@echo %year%-%month%-%day% %hour%:%minute%:%second%
